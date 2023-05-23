4ae5280d1524d076744a3f2e26f26a7f3f6ac345
tayesh21 <zeyadhesham53@gmail.com>
2023-05-23 15:47:43 +0300

    making an example and updatting css colors and footer

--- script.js
+++ script.js
@@ -1,36 +1,8 @@
-const reportSlider = document.getElementById('reportSlider');
-const prevButton = document.getElementById('prevButton');
-const nextButton = document.getElementById('nextButton');
 const reportForm = document.getElementById('reportForm');
+const submittedReport = document.getElementById('submittedReport');
+const verificationMessage = document.getElementById('verificationMessage');
 
-let currentSlide = 0;
-
-prevButton.addEventListener('click', showPreviousReport);
-nextButton.addEventListener('click', showNextReport);
 reportForm.addEventListener('submit', submitReport);
-
-function showPreviousReport() {
-  if (currentSlide === 0) {
-    currentSlide = reportSlider.childElementCount - 1;
-  } else {
-    currentSlide--;
-  }
-  updateSlider();
-}
-
-function showNextReport() {
-  if (currentSlide === reportSlider.childElementCount - 1) {
-    currentSlide = 0;
-  } else {
-    currentSlide++;
-  }
-  updateSlider();
-}
-
-function updateSlider() {
-  const slideHeight = reportSlider.offsetHeight;
-  reportSlider.style.transform = `translateY(-${slideHeight * currentSlide}px)`;
-}
 
 function submitReport(event) {
   event.preventDefault();
@@ -38,19 +10,14 @@ function submitReport(event) {
   const reportType = document.getElementById('reportType').value;
   const reportContent = document.getElementById('reportContent').value;
 
-  const newReport = document.createElement('div');
-  newReport.classList.add('report');
-  newReport.innerHTML = `
-    <h2>Report ${reportSlider.childElementCount + 1}</h2>
+  submittedReport.innerHTML = `
+    <h3>${reportType}</h3>
     <p>${reportContent}</p>
   `;
-  reportSlider.insertBefore(newReport, reportSlider.firstChild);
 
+  verificationMessage.textContent = 'Report submitted successfully!';
+  verificationMessage.style.color = 'green';
+
+  document.getElementById('reportType').value = 'bug';
   document.getElementById('reportContent').value = '';
-
-  currentSlide = 0;
-  updateSlider();
 }
-
-// Initially, update the slider to position the first report
-updateSlider();

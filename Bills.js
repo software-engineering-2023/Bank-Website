d2f88c156ade42d1baaa65c9f1ad596a9b80b6d9
tayesh21 <zeyadhesham53@gmail.com>
2023-05-26 04:53:08 +0300

    bills update

--- Bills.js
+++ Bills.js
@@ -1,30 +1,52 @@
-// Function to handle toggling of details container
-function toggleDetails(detailsContainer) {
-    detailsContainer.classList.toggle('show');
-  }
+// Helper function to toggle visibility of an element
+function toggleVisibility(elementId) {
+    var element = document.getElementById(elementId);
+    if (element.style.display === "none") {
+        element.style.display = "block";
+    } else {
+        element.style.display = "none";
+    }
+}
+
+// Event handler for Show Details button
+function handleShowDetailsClick(event) {
+    var details = event.target.nextElementSibling;
+    toggleVisibility(details.id);
   
-  // Function to simulate paying a bill
-  function payBill(billType) {
-    // Display the submission message
-    const submissionMessage = document.querySelector('.submission-message');
-    submissionMessage.style.display = 'block';
-  }
+    // Dummy data for demonstration
+    var billType = details.getAttribute("data-bill-type");
+    var dueDateElement = details.querySelector(".due-date");
+    var amountElement = details.querySelector(".amount");
   
-  // Attach event listeners to the show details buttons
-  const showDetailsButtons = document.querySelectorAll('.show-details-button');
-  showDetailsButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      const detailsContainer = button.nextElementSibling;
-      toggleDetails(detailsContainer);
-    });
-  });
-  
-  // Attach event listeners to the pay now buttons
-  const payNowButtons = document.querySelectorAll('.pay-now-button');
-  payNowButtons.forEach(button => {
-    button.addEventListener('click', () => {
-      const billType = button.getAttribute('data-bill-type');
-      payBill(billType);
-    });
-  });
-  
+    if (billType === "Gas") {
+        dueDateElement.textContent = "Due Date: January 31, 2023";
+        amountElement.textContent = "Fees: $50";
+    } else if (billType === "Water") {
+        dueDateElement.textContent = "Due Date: February 15, 2023";
+        amountElement.textContent = "Fees: $35";
+    } else if (billType === "Electricity") {
+        dueDateElement.textContent = "Due Date: February 28, 2023";
+        amountElement.textContent = "Fees: $75";
+    } else if (billType === "Telephone") {
+        dueDateElement.textContent = "Due Date: March 10, 2023";
+        amountElement.textContent = "Fees: $45";
+    }
+}
+
+// Event handler for Pay Now button
+function handlePayNowClick() {
+    var submissionMessage = document.querySelector(".submission-message");
+    submissionMessage.style.display = "block";
+}
+
+// Attach event listeners to Show Details buttons
+var showDetailsButtons = document.getElementsByClassName("show-details-button");
+Array.from(showDetailsButtons).forEach(function (button) {
+    button.addEventListener("click", handleShowDetailsClick);
+});
+
+// Attach event listener to Pay Now buttons
+var payNowButtons = document.querySelectorAll(".bill-type button[onclick^='payBill']");
+Array.from(payNowButtons).forEach(function (button) {
+    button.addEventListener("click", handlePayNowClick);
+});
